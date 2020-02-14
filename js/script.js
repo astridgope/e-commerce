167329a40b465b1bbe79ecfcc366e8df14d10f06
astridgope <astridgope@gmail.com>
2020-02-14 20:27:04 +0100

    Dropdowns and footer done. Working on image animations

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ $(document).ready(function () {
     $("#search-field").keypress(function (event) {
         if (event.keyCode == 13) {
             search($("#search-field").val(), 3);
-        }
+        };
     });
 
     function search(searchInput, minLength) {
+        event.preventDefault()
         if (checkLength(searchInput, minLength) == true) {
             console.log("success");
             msgModal("Resultats de la cerca", "0");
@@ -20,51 +21,56 @@ $(document).ready(function () {
             console.log("error");
             msgModal("Error", "Introdueix una paraula de com a mínim 3 caràcters.");
         };
-    }
+    };
 
 
     // USER REGISTRATION
     $("#email-register").change(function () {
+        event.preventDefault()
         let email = $(this).val();
         if (checkEmail(email) == false) {
             invalidFn($("#email-register"), $("#email-register-feedback"), "Format invàlid.");
         } else {
             validFn($("#email-register"), $("#email-register-feedback"));
-        }
+        };
     });
 
     $("#password-register").change(function () {
+        event.preventDefault()
         let password = $(this).val();
         if (checkLength(password, 8) == false) {
             invalidFn($("#password-register"), $("#password-register-feedback"), "8 caràcters com a mínim.");
         } else {
             validFn($("#password-register"), $("#password-register-feedback"));
-        }
+        };
     });
 
     $("#re-password-register").change(function () {
+        event.preventDefault()
         let rePassword = $(this).val();
         let password = $("#password-register").val();
         if (confirmPassword(rePassword, password) == false) {
             invalidFn($("#re-password-register"), $("#re-password-register-feedback"), "No coincideix.");
         } else {
             validFn($("#re-password-register"), $("#re-password-register-feedback"));
-        }
+        };
     });
 
     $("#province-register").change(function () {
+        event.preventDefault()
         let province = $(this).children("option:selected").val();
         let defaultOption = $(this).children("option:first-child").val();
         if (checkProvince(province, defaultOption) == false) {
             invalidFn($("#province-register"), $("#province-feedback"), "Selecciona una província.");
         } else {
             validFn($("#province-register"), $("#province-register-feedback"));
-        }
+        };
     });
 
     var signinData = [];
 
     $("#register-btn").click(function () {
+        event.preventDefault()
         let email = $("#email-register");
         let password = $("#password-register");
         let rePassword = $("#re-password-register");
@@ -84,43 +90,27 @@ $(document).ready(function () {
     });
 
     function checkLength(input, minLength) {
-        if (input.length < minLength) {
-            return false;
-        } else {
-            return true;
-        };
+        return (input.length >= minLength)
     };
 
     function checkEmail(email) {
         let emailRegex = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
-        if (!(emailRegex.test(email))) {
-            return false;
-        } else {
-            return true;
-        };
+        return (emailRegex.test(email))
     };
 
     function confirmPassword(rePassword, password) {
-        if (rePassword != password) {
-            return false;
-        } else {
-            return true;
-        };
+        return (rePassword == password)
     };
 
     function checkProvince(province, defaultOption) {
-        if (province == defaultOption) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+        return (province != defaultOption)
+    };
 
     function invalidFn(field, fieldFeedback, msg) {
         field.addClass("is-invalid");
         fieldFeedback.addClass("invalid-feedback");
         fieldFeedback.html(msg);
-    }
+    };
 
     function validFn(field, fieldFeedback) {
         field.removeClass("is-invalid");
@@ -128,19 +118,16 @@ $(document).ready(function () {
         fieldFeedback.addClass("valid-feedback");
         field.addClass("is-valid");
         fieldFeedback.html("");
-    }
+    };
 
     function checkValidClass(field) {
-        if (field.hasClass("is-valid")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+        return (field.hasClass("is-valid"))
+    };
 
 
     // USER LOGIN
     $("#email-signin").change(function () {
+        event.preventDefault()
         let email = $(this).val();
         if (email != signinData[0]) {
             invalidFn($("#email-signin"), $("#email-signin-feedback"), "Aquest email no està registrat.");
@@ -150,6 +137,7 @@ $(document).ready(function () {
     });
 
     $("#password-signin").change(function () {
+        event.preventDefault()
         let password = $(this).val();
         if (password != signinData[1]) {
             invalidFn($("#password-signin"), $("#password-signin-feedback"), "Contrasenya incorrecta.");
@@ -159,6 +147,7 @@ $(document).ready(function () {
     });
 
     $("#signin-btn").click(function () {
+        event.preventDefault()
         let email = $("#email-signin").val();
         let password = $("#password-signin").val();
         if ((email == signinData[0]) && (password == signinData[1])) {
@@ -195,7 +184,7 @@ $(document).ready(function () {
         openModal($("#msg-modal"));
         $("#msg-modal h5").html(title);
         $("#msg-modal p").html(msg);
-    }
+    };
 
     function openModal(idModal) {
         idModal.modal("toggle");
